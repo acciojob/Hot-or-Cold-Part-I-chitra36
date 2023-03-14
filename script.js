61cec417a7b17da4d5296c4dd18346f55cd11a50
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-15 02:36:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ var btn=document.getElementById("btn");
 var numPtag=document.getElementById("num");
 var res=document.getElementById("respond");
 var inputValue=document.getElementById("guess").value;
-function  randomNumGenerator(){
-	randomNum=Math.floor(Math.random()*41)-20;
+
+
+function  randomNumGenerator()
+{
+	randomNum=Math.floor(Math.random()*54)-14;
 	document.getElementById("num").innerText=randomNum;
 	guessNum();
 	print();
 }
-	function guessNum(){
+	function guessNum()
+	{
 		input=Number(document.getElementById("guess").value);
 	}
-function print(){
-	if(Math.abs(randomNum-input)<=5){
+function print()
+{
+	if(Math.abs(randomNum-input)<=5)
+	{
 		document.getElementById("respond").innerText="Hot";
 	}
-	else{
+else{
 		document.getElementById("respond").innerHTML="Cold";
 	}
 }
